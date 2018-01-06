9d11f279c9c9b2ca22617da058d8d10316993538
unknown <clz@clz.me>
2018-01-06 15:15:47 +0800

    首次上传

---
+++ src/providers/converter/converter.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the ConverterProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ConverterProvider {
+
+  constructor() {
+    console.log('Hello ConverterProvider Provider');
+  }
+
+}
